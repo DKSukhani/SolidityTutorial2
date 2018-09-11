e351583839c8e4e5ab6bd36c9dec8ce71b3de606
Dipesh Sukhani <dipeshsukhani@gmail.com>
2018-09-11 18:31:39 +0800

    Updated the code for Web3@1.0.0.beta.26

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,37 +1,39 @@
 const assert = require('assert');
-const ganache = require ('ganache-cli');
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require ('../compile')
+const { interface, bytecode } = require('../compile');
 
 let accounts;
-let inboxContract;
-// const INITIAL_MESSAGE = "Hi Dipesh!"
+let inbox;
 
-beforeEach (async () => {
-// Get a list of all accounts
-accounts = await web3.eth.getAccounts(function(err, res){
-	 console.log(res)});
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-// Use one of those accounts to deploy the contract
-
-	inboxContract = await web3.eth.contract(JSON.parse(interface)).new(["Hi Dipesh!"], { data: bytecode,from: accounts[0], gas: "1000000"});
-
+  // Use one of those accounts to deploy
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
 
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
 
-describe('Inbox', ()=> {
-	it('deploys a contract', () => {
-		assert.ok(inboxContract.options.address);
-	});
-	// in this test we are checking the default message
-	it('it has a default message', async () => {
-		const message  = await inboxContract.methods.message().call();
-		console.log(message);
-		assert.equal(message, "Hi Dipesh!")
-	});
-
-	// in this test we will modify the message and test if
-
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 });
